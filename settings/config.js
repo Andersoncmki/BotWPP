8f1e8d52b84c2483aab01d8b6e2522b2086b37fa
slash2018 <victor.slash13@hotmail.com>
2021-02-27 02:50:27 -0300

    bot

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'BOTVictor',
+        ownerName: 'ADMVictor',
+        youtube: 'https://www.youtube.com/channel/UC5XgXdQO1_T9ChVLgVAiI3A',
+        instagram: 'https://www.instagram.com/gamerlagado/',
+}
